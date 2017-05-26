9b36e609b31085b3bccc8ae0150833ccc3db5ce0
Sergey Petrunin <neyasut@gmail.com>
2017-05-26 23:20:05 +0300

    add validations add manager model

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,3 +1,5 @@
 class Vacation < ApplicationRecord
   belongs_to :worker
+
+  validates_presence_of :worker, :start_date, :end_date
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,3 +1,7 @@
 class Worker < ApplicationRecord
   has_secure_password
+
+  has_many :vacations
+
+  validates_presence_of :name, :email
 end
