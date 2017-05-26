d42796c0d517fabbd88cad9f7b5bb3ccbf89a5f6
Sergey Petrunin <neyasut@gmail.com>
2017-05-26 23:21:32 +0300

    add generators config

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module AltTabApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
